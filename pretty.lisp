9ed6605544f6d360a5b887009c6ab8b99c21f200
Aaron S. Jackson <aaron@aaronsplace.co.uk>
2019-02-07 09:41:43 +0000

    mat multiply works, might need to switch to col first ordering.

--- pretty.lisp
+++ pretty.lisp
@@ -27,64 +27,70 @@
   (setf (slot-value m 'ptr-c)
 	(cffi:foreign-alloc
 	 :float
-	 :initial-contents data)))
+	 :initial-contents data))
+  m)
 
 (defmethod push-matrix (m)
   "push c data to cuda data"
   (if (not (ptr-gpu m))
-      (setf
-	  (slot-value m 'ptr-gpu)
-	  (cffi:foreign-alloc :pointer))
-      (cublasAlloc
-       (* (rows m) (cols m)) 4
-       (slot-value m 'ptr-gpu)))
+      (progn (setf (ptr-gpu m)
+		   (cffi:foreign-alloc :pointer))
+	     (cublasAlloc (* (rows m) (cols m))
+			  (cffi:foreign-type-size ':float)
+			  (ptr-gpu m))))
   (cublasSetMatrix (rows m) (cols m)
-		 (cffi:foreign-type-size ':float)
-		 (ptr-c m) (rows m)
-		 (cffi:mem-ref (slot-value m 'ptr-gpu) ':pointer) (cols m)))
+		   (cffi:foreign-type-size ':float)
+		   (ptr-c m) (rows m)
+		   (cffi:mem-ref (ptr-gpu m) ':pointer) (cols m))
+  m)
 
 (defmethod pull-matrix (m)
   "pull cuda data to c data"
   (cublasGetMatrix (rows m) (cols m)
-		 (cffi:foreign-type-size ':float)
-		 (cffi:mem-ref (ptr-gpu m) ':pointer)
-		 (rows m) (slot-value m 'ptr-c) (cols m)))
+		   (cffi:foreign-type-size ':float)
+		   (cffi:mem-ref (ptr-gpu m) ':pointer)
+		   (rows m) (ptr-c m) (cols m))
+  m)
 
 (defmethod print-matrix (m)
   "prints a matrix, uh, as a list for now"
   (print (loop for i from 0 below (* (rows m) (cols m))
 	    collect (cffi:mem-aref (ptr-c m) :float i)))
-  T)
+  m)
 
-;; (defmethod multiply-matrix ((A matrix) (B matrix))
-;;   "A * B"
-;;   (let (Z (make-instance 'matrix :rows (rows A) :cols (cols B)))
-;;     (set-matrix Z (make-array
-;; 		   (* (rows A) (cols B))
-;; 		   :initial-element 0.0))
-;;     (push-matrix Z)
-;;     (cublasSgemm 78 78 (cols A) (rows B) (rows A) 1.0
-;; 		 (cffi:mem-ref (ptr-gpu A) ':pointer) (rows A)
-;; 		 (cffi:mem-ref (ptr-gpu B) ':pointer) (cols A) 1.0
-;; 		 (cffi:mem-ref (slot-value Z 'ptr-gpu) ':pointer)))
-;;   Z)
+;; The dimensions may be wrong, haven't really thought about them much
+;; since I'm only trying a 3x3 matrix at the moment.
+(defmethod multiply-matrix ((A matrix) (B matrix))
+  "A"
+  (let ((Z (make-instance 'matrix :rows (rows A) :cols (cols B))))
+    (set-matrix Z (make-array (* (rows A) (cols A))
+			      :initial-element 0.0))
+    (push-matrix Z)
+    (cublasSgemm 78 78 (cols A) (rows B) (rows A) 1.0
+		 (cffi:mem-ref (ptr-gpu A) ':pointer) (rows A)
+		 (cffi:mem-ref (ptr-gpu B) ':pointer) (rows A) 1.0
+		 (cffi:mem-ref (ptr-gpu Z) ':pointer) (cols A))
+    Z))
 
 
+(defparameter *cublas* (cffi:foreign-alloc :pointer))
+(cublasCreate_v2 *cublas*)
 
 (defvar A (make-instance 'matrix :rows 3 :cols 3))
 (defvar B (make-instance 'matrix :rows 3 :cols 3))
 
-(set-matrix A #(2.0 2.0 2.0
-		2.0 2.0 2.0
-		2.0 2.0 2.0))
-(set-matrix B #(1.0 0.0 0.0
-		0.0 1.0 0.0
-		0.0 0.0 1.0))
-(push-matrix A)
-(pull-matrix A)
-(print-matrix A)
+(set-matrix A #(1.0 2.0 3.0
+		4.0 5.0 6.0
+		7.0 8.0 9.0))
+(set-matrix B #(1.0 2.0 1.0
+		2.0 1.0 2.0
+		1.0 2.0 1.0))
 
-; This bit does not work yet.
-;(defvar C (multiply-matrix A B))
-;(pull-matrix C)
-;(print-matrix C)
+(push-matrix A)
+(push-matrix B)
+
+;; Hmm output is transposed. Might need to switch to column first
+;; indexing.
+(defvar C (multiply-matrix A B))
+(pull-matrix C)
+(print-matrix C)
