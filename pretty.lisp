61ed07906dedf3076b367b0df9d7dade4762dbb1
Aaron S. Jackson <aaron@aaronsplace.co.uk>
2019-02-13 20:57:02 +0000

    Made cpu and gpu functions more automatic

--- pretty.lisp
+++ pretty.lisp
@@ -14,56 +14,67 @@
    (cols
     :initarg :cols
     :accessor cols)
-   (ptr-c
-    :accessor ptr-c
+   (ptr-cpu
+    :accessor ptr-cpu
     :initform nil)
    (ptr-gpu
     :accessor ptr-gpu
-    :initform nil)))
+    :initform nil)
+   (current-ptr ;; specifies whether the latest copy is on cpu or gpu
+    :accessor current-ptr
+    :initform 'cpu)))
 
 (defmethod set-data ((m matrix) data)
   "set data of matrix"
-  (setf (slot-value m 'ptr-c)
+  (setf (slot-value m 'ptr-cpu)
 	(cffi:foreign-alloc
 	 :float
 	 :initial-contents data))
+  (setf (current-ptr m) 'cpu)
   m)
 
 (defmethod gpu ((m matrix))
   "push c data to cuda data"
-  (if (not (ptr-gpu m))
-      (progn (setf (ptr-gpu m)
-		   (cffi:foreign-alloc :pointer))
-	     (cublasAlloc (* (rows m) (cols m))
-			  (cffi:foreign-type-size ':float)
-			  (ptr-gpu m))))
-  (cublasSetMatrix (rows m) (cols m)
-		   (cffi:foreign-type-size ':float)
-		   (ptr-c m) (rows m)
-		   (cffi:mem-ref (ptr-gpu m) ':pointer) (cols m))
+  (if (eq (current-ptr m) 'cpu)
+      (progn (if (not (ptr-gpu m))
+		 (progn (setf (ptr-gpu m)
+			      (cffi:foreign-alloc :pointer))
+			(cublasAlloc (* (rows m) (cols m))
+				     (cffi:foreign-type-size ':float)
+				     (ptr-gpu m))))
+	     (cublasSetMatrix (rows m) (cols m)
+			      (cffi:foreign-type-size ':float)
+			      (ptr-cpu m) (rows m)
+			      (cffi:mem-ref (ptr-gpu m) ':pointer) (cols m))))
+  (setf (current-ptr m) 'gpu)
   m)
 
 (defmethod cpu ((m matrix))
   "pull cuda data to c data"
-  (cublasGetMatrix (rows m) (cols m)
-		   (cffi:foreign-type-size ':float)
-		   (cffi:mem-ref (ptr-gpu m) ':pointer)
-		   (rows m) (ptr-c m) (cols m))
+  (if (eq (current-ptr m) 'gpu)
+      (cublasGetMatrix (rows m) (cols m)
+		       (cffi:foreign-type-size ':float)
+		       (cffi:mem-ref (ptr-gpu m) ':pointer)
+		       (rows m) (ptr-cpu m) (cols m)))
+  (setf (current-ptr m) 'cpu)
   m)
 
 (defmethod print-object ((m matrix) stream)
   "prints a matrix, uh, as a list for now"
+  (cpu m)
   (format stream "~$" (loop for i from 0 below (* (rows m) (cols m))
-		    collect (cffi:mem-aref (ptr-c m) :float i)))
+			 collect (cffi:mem-aref (ptr-cpu m) :float i)))
   m)
 
 ;; The dimensions may be wrong, haven't really thought about them much
 ;; since I'm only trying a 3x3 matrix at the moment.
 (defmethod multiply ((A matrix) (B matrix))
   "A"
+  (gpu A)
+  (gpu B)
   (let ((Z (make-instance 'matrix :rows (rows A) :cols (cols B))))
     (set-data Z (make-array (* (rows A) (cols A))
-			      :initial-element 0.0))
+			    :initial-element 0.0))
     (gpu Z)
     (cublasSgemm 78 78 (cols A) (rows B) (rows A) 1.0
 		 (cffi:mem-ref (ptr-gpu A) ':pointer) (rows A)
@@ -74,13 +85,13 @@
 (defvar A (make-instance 'matrix :rows 3 :cols 3))
 (defvar B (make-instance 'matrix :rows 3 :cols 3))
 
-(gpu (set-data A #(1.0 2.0 3.0
-		   4.0 5.0 6.0
-		   7.0 8.0 9.0)))
-(gpu (set-data B #(1.0 2.0 1.0
-		   2.0 1.0 2.0
-		   1.0 2.0 1.0)))
+(set-data A #(1.0 2.0 3.0
+	      4.0 5.0 6.0
+	      7.0 8.0 9.0))
+(set-data B #(1.0 2.0 1.0
+	      2.0 1.0 2.0
+	      1.0 2.0 1.0))
 
 ;; Hmm output is transposed. Might need to switch to column first
 ;; indexing.
-(print (cpu (multiply (gpu A) (gpu B))))
+(print (multiply A B))
